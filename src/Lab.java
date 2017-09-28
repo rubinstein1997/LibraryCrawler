0a1bd9f304ed5e1307e9fd54dfb2e56568f65500
rubinstein1997 <liuyunlong0908@outlook.com>
2017-09-28 08:43:04 -0700

    two

--- src/Lab.java
+++ src/Lab.java
@@ -1,5 +1,9 @@
 import java.io.*;
 import java.net.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -13,9 +17,11 @@ public class Lab {
 
         String charset = "UTF-8";
 
+        DB db = new DB();
+
         for(long i = 2015013915; i <= 2015013915; i++) {
 
-            String param = new Long(i).toString();
+            String param = Long.toString(i);
 
             String query = String.format("user=%s&pw=%s", URLEncoder.encode(param, charset), URLEncoder.encode(param, charset));
 
@@ -35,24 +41,78 @@ public class Lab {
 
             String name = "【(\\S+)】";
 
+            String libnum = "\\d{8}";
+
             Pattern p = Pattern.compile(regex);
 
             Pattern namep = Pattern.compile(name);
 
+            Pattern libnume = Pattern.compile(libnum);
+            String realname = null;
+            String libnumeb = null;
+            String Id = null;
             while ((line = bufr.readLine()) != null) {
 //                System.out.println(line);
                 Matcher m = p.matcher(line);
                 if (m.find()) {
                     System.out.println("身份证" + m.group());
+                    Id = m.group();
                 }
                 Matcher n = namep.matcher(line);
                 if (n.find()) {
                     System.out.print("学号 " + i + "\t" + "姓名" + n.group(1) +"\t");
+                    realname = n.group(1);
                 }
+                Matcher j = libnume.matcher(line);
+                if(j.find())
+                    libnumeb = j.group();
+
+
             }
+            db.insert(i,realname,libnumeb,Id);
         }
     }
 
 }
+
+class DB {
+
+    static final String DB_URL = "jdbc:mysql://localhost:3306/Lib?useUnicode=true&characterEncoding=UTF-8";
+
+    //set database username password
+    static final String USER = "root";
+    static final String PASS = "123456";
+
+    Connection conn = null;
+    Statement stmt = null;
+
+    //insert data
+    public  void insert(long data1, String data2, String data3, String data4) {
+
+        try{
+            //Register JDBC driver, maybe userful
+            //Class.forName("com.mysql.jdbc.Driver");
+
+            //Open a connection
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            //Execute a query
+            stmt = conn.createStatement();
+
+            String sql = "INSERT INTO lib " +
+                    "VALUES (" + "134"+",'"+ data2+"'," + data3 +",'"+data4+"')";
+
+            //ensure sql
+//            System.out.println(sql);
+
+            stmt.executeUpdate(sql);
+        }
+        catch(Exception e){
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        }
+    }
+}
+
 
 
